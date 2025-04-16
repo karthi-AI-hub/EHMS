12abbe7f9fb0af0eb135b8b146a40da3937af401
karthi <karthikeyanks463@gmail.com>
2025-04-16 09:39:19 +0530

    Completed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_API_BASE_URL, {
+    path: "/ehms/api/socket.io",
     transports: ["websocket"],
+    withCredentials: true
 });
 
 export default socket;
